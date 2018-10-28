62c87b6a55a09b89a4ad3ac816162a33a5689c31
Christoph Klocker <christoph@vedanova.com>
2018-10-28 09:59:20 +0100

    registration of nft working

--- nodejs_backend_hapy/services/create_nft_endpoint.js
+++ nodejs_backend_hapy/services/create_nft_endpoint.js
@@ -1,9 +1,8 @@
 const ApiClient = require('./api_client');
 const neon_js = require('@cityofzion/neon-js');
 
-const privateKey = "KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr";
 const contractScript = "8f02b02cc528ee5de89d5e27090ed4fa7dffaa19"
-
+const addressScriptHash = "23ba2703c53263e8d6e522dc32203339dcd8eee9"
 var Neon = neon_js.default;
 
 
@@ -29,78 +28,38 @@ class CreateNftEndPoint {
                 console.log("Invoke error", error)
             });
 
-        this.response.data = {
+        let data = {
             "totalSupply": totalSupply,
             "msg": "Successfully Done",
             "error": ""
         };
-        this.response.code = 200;
-        return this.response;
+
+        return this.successfulResponse(data)
     }
 
     // register a new motor as NFT
     async register() {
+        const apiClient = new ApiClient()
+
         let params = {
             manufacturer_id: "12345",
             mileage: "0"
         }
+
         let parameterString = JSON.stringify(params);
-        console.log(parameterString);
-
-        const networkConfig = {
-            name: 'PrivateNet',
-            extra: {
-                neoscan: 'http://localhost:4000/api/main_net',
-                neonode: "http://localhost:30333"
-            }
-        }
-
-        const privateNet = new neon_js.rpc.Network(networkConfig)
-        Neon.add.network(privateNet)
-        console.log('private net setup')
-        const neoscanInstance = new neon_js.api.neoscan.instance('PrivateNet');
-        console.log('configuration')
-
-
         let uri = Neon.u.str2hexstring("/mdcva/1233")
         let parameterStringHex = Neon.u.str2hexstring(parameterString)
 
-        console.log('hex hex')
+        let args = [addressScriptHash,
+            parameterStringHex,
+            uri
+        ]
 
+        const data = await apiClient.doInvoke("mintToken", args)
+        return this.successfulResponse(data)
+    }
 
-        const config = {
-            api: neoscanInstance,
-            script: {
-                scriptHash: contractScript,
-                operation: 'mintToken',
-                args: ["23ba2703c53263e8d6e522dc32203339dcd8eee9",
-                    parameterStringHex,
-                    uri
-                ]
-            },
-            account: new neon_js.wallet.Account(privateKey),
-            gas: 1
-        };
-
-        let data = null
-        console.log('invoking')
-        let invoke = await Neon.doInvoke(config)
-            .then(res => {
-                data = {
-                    "response": res,
-                    "msg": "Successfully Done",
-                    "error": ""
-                }
-            })
-            .catch(error => {
-                data = {
-                    "response": error,
-                    "msg": "Invocation Error",
-                    "error": "Invocation Error"
-                }
-                console.log("Invoke error", error)
-            });;
-
+    successfulResponse(data) {
         this.response.data = data;
         this.response.code = 200;
         return this.response;
